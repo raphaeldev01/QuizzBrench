fc035453a573f87d7e3740050e4c07c041e4e548
Raphael Developer <raphael.developer@hotmail.com>
2025-03-22 00:58:10 -0300

    update datas

--- database/UsersDB.js
+++ database/UsersDB.js
@@ -1,18 +1,34 @@
 const Database = require("./databaseConfig")
-const { getDoc, where, doc, collection, query, getDocs } = require("firebase/firestore")
+const uniqid = require('uniqid') 
+const { getDoc, where, doc, collection, query, getDocs, setDoc, addDoc } = require("firebase/firestore")
+
+const collectionUsers = collection(Database, "users")
 
 const NewUser = async (userInfos) => {  
-  collection(Database, "user")
-    // const queryUser = query(collection(Database, "user"), where("user", "==", userInfos.user))
-
     const snapQueryMail = (await getDocs(query(collection(Database, "users"), where("email", "==", userInfos.email)))).docs.length
-    const snapQueryUser = (await getDocs(query(collection(Database, "users"), where("username", "==", userInfos.user)))).docs.length
+    const snapQueryUser = (await getDocs(query(collection(Database, "users"), where("userId", "==", userInfos.userId)))).docs.length
 
     if(snapQueryMail > 0) return {error: true, msg: "Email unavailable!"} 
-    if(snapQueryUser > 0) return {error: true, msg: "Username unavailable!"} 
+    if(snapQueryUser > 0) return {error: true, msg: "userID unavailable!"}
+    
+    const secretToken = `${uniqid()}${uniqid()}${uniqid()}${uniqid()}${uniqid()}${uniqid()}`
+    const date = new Date()
+
+    const obj = {
+      secretToken,
+      ...userInfos,
+      date, 
+    }  
+  
+    try{
+      addDoc(collectionUsers, obj)
+      return {error: false, msg: "User added!"}
+    }catch (err) {
+      return {error: true, msg: err}
+    }
 }
 
 NewUser({
-    email:"teste@gmail.com",
-    user: "raphael"
-})
+    email:"teste431131@gmail.com",
+    userId: "raphael112"
+}).then(r => console.log(r))
